9c4fd1ad76dfc18a19903388a0cac85ac0d20a9f
jamatov <mirabdullajamatov@gmail.com>
2023-06-18 11:08:40 +0500

    bismillah

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <nav className='Navbar'>
+      <div className="container">
+        
+      </div>
+    </nav>
+  )
+}
