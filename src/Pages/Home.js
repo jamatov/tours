deeb697d0b68713c2a8b484bc1775e9cafae4998
jamatov <mirabdullajamatov@gmail.com>
2023-06-18 15:24:40 +0500

    transport info start

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Homehead from '../Components/Home/Homehead'
 import ToursCards from '../Components/Home/ToursCards'
+import TransportInfo from '../Components/Home/TransportInfo'
 
 export default function Home() {
   return (
     <section className='Home'>
       <Homehead/>
       <ToursCards/>
+      <TransportInfo/>
     </section>
   )
 }
